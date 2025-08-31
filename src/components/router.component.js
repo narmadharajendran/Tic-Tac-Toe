37227de0ae2aa0440a25b523391f45c2646dec96
Rajendran <Narmadha.Rajendran@agilysys.com>
2025-08-31 18:47:52 +0530

    Fix router for GitHub Pages deployment

--- src/components/router.component.js
+++ src/components/router.component.js
@@ -7,7 +7,7 @@ import Home from "./home/home";
 const RouterComponent = () =>{
     return(
         <Routes>
-            <Route path="/" element={<Navigate to="/Home" replace />} />
+            <Route path="/" element={<Navigate to="/Home" replace />} basename={process.env.PUBLIC_URL}/>
             <Route path="/Home" element={<Home />} exact/>
             <Route path="/Single-player" element={<SinglePlayer />} exact/>
             <Route path="/Multi-player" element={<MultiplePlayer />} exact/>
