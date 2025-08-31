69ba9c84cab7ae22b7f86ff8de809b5b57bd73d5
Rajendran <Narmadha.Rajendran@agilysys.com>
2025-08-31 22:28:04 +0530

    Fix routing with HashRouter for GitHub Pages

--- src/components/router.component.js
+++ src/components/router.component.js
@@ -1,17 +1,20 @@
 import {React} from "react";
-import { Navigate, Route, Routes } from "react-router";
+// import { Navigate, Route, Routes } from "react-router";
 import SinglePlayer from "./single-player-board/single-player-board";
 import MultiplePlayer from "./multiple-player-board/multiple-player-board";
 import Home from "./home/home";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const RouterComponent = () =>{
     return(
-        <Routes>
-            <Route path="/" element={<Navigate to="/Home" replace />} basename={process.env.PUBLIC_URL}/>
-            <Route path="/Home" element={<Home />} exact/>
-            <Route path="/Single-player" element={<SinglePlayer />} exact/>
-            <Route path="/Multi-player" element={<MultiplePlayer />} exact/>
-        </Routes>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/Home" replace />} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/Single-player" element={<SinglePlayer />} />
+                <Route path="/Multi-player" element={<MultiplePlayer />} />
+            </Routes>
+        </HashRouter>
     )
 }
 export default RouterComponent
